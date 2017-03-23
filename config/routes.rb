f8d1a3d0ad34f0baba8d4de7199dede1e1224e03
scaperoth <scaperoth@gmail.com>
2017-03-23 15:20:21 -0400

    fixing routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
 
     put 'apartments/:id/status/:status' => 'apartments#changestatus'
 
-    authenticated :user do
-        root to: 'apartments#index'
-    end
-
     root 'pages#index'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
