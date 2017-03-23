4178c9fefae51022536ce7b495a958a936a921be
scaperoth <scaperoth@gmail.com>
2017-03-23 15:04:09 -0400

    UI updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :apartments
-  
-  put 'apartments/:id/status/:status' => 'apartments#changestatus'
-  
-  root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    devise_for :users
+    resources :apartments
+
+    put 'apartments/:id/status/:status' => 'apartments#changestatus'
+
+    authenticated :user do
+        root to: 'apartments#index'
+    end
+
+    root 'pages#index'
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
